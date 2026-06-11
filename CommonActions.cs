584deb34978c8af37348b6bd5f2c60a18b7c6ee2
kvaibhavi <62807550+kvaibhavi@users.noreply.github.com>
2026-06-11 20:11:51 +0530

    Add CommonActions enum with SearchBooks action

---
+++ CommonActions.cs
@@ -0,0 +1,11 @@
+public enum CommonActions
+{
+    Repeat = 0,
+    Select = 1,
+    Quit = 2,
+    SearchPatrons = 4,
+    RenewPatronMembership = 8,
+    ReturnLoanedBook = 16,
+    ExtendLoanedBook = 32,
+    SearchBooks = 64
+}
